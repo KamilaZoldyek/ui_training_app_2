3d586042444b3dd6efbc6925a8fcee7478ea0334
KamilaZoldyek <kmilawicked@hotmail.com>
2018-12-21 10:00:05 -0200

    activities, layout changes

--- app/src/main/java/com/example/kamilazoldyek/ui_training_app_2/MainActivity.java
+++ app/src/main/java/com/example/kamilazoldyek/ui_training_app_2/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.kamilazoldyek.ui_training_app_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button signin, signup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        signin = findViewById(R.id.signIN);
+        signup = findViewById(R.id.signUP);
+
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignInScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kamilazoldyek/ui_training_app_2/SignInScreen.java
+++ app/src/main/java/com/example/kamilazoldyek/ui_training_app_2/SignInScreen.java
@@ -1,9 +1,15 @@
 package com.example.kamilazoldyek.ui_training_app_2;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-public class SignInScreen extends Activity {
+public class SignInScreen extends AppCompatActivity {
 
-
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
 }
